e41495f843dbb0aec5397c3138ffd46a4afb38a9
Chathura Colombage <chathura.colombage@qlearsite.com>
2018-08-19 13:31:11 +0100

    add rename swap test

--- src/rename.rs
+++ src/rename.rs
@@ -2,6 +2,7 @@ use std::collections::hash_map::DefaultHasher;
 use std::collections::{HashMap, HashSet};
 use std::fs::{self, metadata};
 use std::hash::{Hash, Hasher};
+use std::path::PathBuf;
 
 pub fn bulk_rename(froms: &Vec<String>, tos: &Vec<String>, is_demo: bool) -> Result<i32, String> {
     if froms.len() != tos.len() {
@@ -28,6 +29,14 @@ pub fn bulk_rename(froms: &Vec<String>, tos: &Vec<String>, is_demo: bool) -> Res
     }
 }
 
+fn replace_filename(file_path: &str, name: &str) -> String {
+    let mut path = PathBuf::new();
+    path.push(file_path);
+    path.pop();
+    path.push(name);
+    path.to_str().unwrap().to_string()
+}
+
 fn with_temporary_moves(renames: &Vec<(Rename, Rename)>) -> Vec<Rename> {
     use uuid::Uuid;
 
@@ -35,9 +44,9 @@ fn with_temporary_moves(renames: &Vec<(Rename, Rename)>) -> Vec<Rename> {
     for (ref x, ref y) in renames {
         let temp_file_name = format!("{}", Uuid::new_v4());
 
-        non_conflicting.push(x.with_to(&temp_file_name));
+        non_conflicting.push(x.with_to(&replace_filename(&x.to, &temp_file_name)));
         non_conflicting.push(y.clone());
-        non_conflicting.push(x.with_from(&temp_file_name));
+        non_conflicting.push(x.with_from(&replace_filename(&x.from, &temp_file_name)));
     }
 
     non_conflicting
@@ -158,4 +167,106 @@ impl Rename {
             is_dir: self.is_dir,
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use std::fs::{self, File};
+    use std::io::prelude::*;
+    use std::panic;
+    use std::path::PathBuf;
+
+    // Run a test with given setup before the test and teardown after the test.
+    // Should ensure that setup and teardown code does not panic
+    #[allow(dead_code)]
+    fn run_test_with_setup<S, C, T>(setup: S, teardown: C, test: T) -> ()
+    where
+        S: FnOnce() -> (),
+        C: FnOnce() -> (),
+        T: FnOnce() -> () + panic::UnwindSafe,
+    {
+        setup();
+        let result = panic::catch_unwind(|| test());
+        teardown();
+
+        assert!(result.is_ok());
+    }
+
+    struct RenameTestSetup {
+        dir: String,
+        file_contents: Vec<(String, String)>,
+    }
+
+    impl RenameTestSetup {
+        fn with_temp_dir(d: &str) -> RenameTestSetup {
+            let mut temp_path = env::temp_dir();
+            temp_path.push(d);
+            RenameTestSetup {
+                dir: temp_path.to_str().unwrap().to_string(),
+                file_contents: vec![],
+            }
+        }
+
+        fn init(&self) -> &RenameTestSetup {
+            fs::remove_dir_all(&self.dir).unwrap();
+            fs::create_dir(&self.dir).unwrap();
+            self
+        }
+
+        fn full_path(&self, file: &str) -> PathBuf {
+            let mut temp_path: PathBuf = PathBuf::new();
+            temp_path.push(&self.dir);
+            temp_path.push(file);
+            temp_path
+        }
+
+        fn add_file(&mut self, file: &str, contents: &str) -> &mut RenameTestSetup {
+            let file_path = self.full_path(file);
+            match fs::write(&file_path, contents) {
+                Ok(_) => self.file_contents.push((
+                    file_path.to_str().unwrap().to_string(),
+                    contents.to_string(),
+                )),
+                Err(_) => (),
+            }
+
+            self
+        }
+    }
+
+    fn read_all(file: &str) -> String {
+        let mut contents = String::new();
+
+        let mut f = File::open(file).expect("file not found");
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
+
+        contents
+    }
+
+    #[test]
+    fn rename_swap_test() {
+        let mut setup = RenameTestSetup::with_temp_dir("rename_test");
+        setup.init();
+        setup.add_file("A.txt", "hello");
+        setup.add_file("B.txt", "hi");
+
+        println!("Files: {:?}", setup.file_contents);
+
+        let (ref file_a, ref contents_a) = setup.file_contents[0];
+        let (ref file_b, ref contents_b) = setup.file_contents[1];
+
+        let res = super::bulk_rename(
+            &vec![file_a.to_owned(), file_b.to_owned()],
+            &vec![file_b.to_owned(), file_a.to_owned()],
+            false,
+        );
+
+        assert!(res.is_ok());
+
+        assert_eq!(read_all(&file_a), contents_b.to_owned());
+        assert_eq!(read_all(&file_b), contents_a.to_owned());
+    }
+
 }
